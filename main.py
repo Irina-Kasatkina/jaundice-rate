927ab7e1c864a783a11bb0480ced8df928e3c329
Irina Kasatkina <kasatkina235@gmail.com>
2023-05-02 17:09:55 +0300

    Calculate jaundice rating of article

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import asyncio
 import platform
 
 import aiohttp
+import pymorphy2
 
 import adapters
+from text_tools import calculate_jaundice_rate, split_by_words
 
 
 async def fetch(session, url):
@@ -13,10 +15,16 @@ async def fetch(session, url):
 
 
 async def main():
+    morph = pymorphy2.MorphAnalyzer()
+    charged_words = ['чрезвычайно', 'фантастический']
+
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, 'https://inosmi.ru/20211116/250914886.html')
         clean_text = adapters.SANITIZERS['inosmi_ru'](html, plaintext=True)
-        print(clean_text)
+        article_words = split_by_words(morph, clean_text)
+        jaundice_rating = calculate_jaundice_rate(article_words, charged_words)
+        print(f'Рейтинг: {jaundice_rating}')
+        print(f'Слов в статье: {len(article_words)}')
 
 
 if __name__ == '__main__':
