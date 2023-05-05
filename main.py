f27a4a940af1b4fa891186258ca291cb7c54523e
Irina Kasatkina <kasatkina235@gmail.com>
2023-05-05 16:25:28 +0300

    Compare objectivity of articles

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from pathlib import Path
 
 import aiohttp
 import pymorphy2
+from anyio import sleep, create_task_group
 
 import adapters
 from exceptions import DirectoryNotFound
 from text_tools import calculate_jaundice_rate, split_by_words
+
+
+TEST_ARTICLES = [
+    'https://inosmi.ru/20211116/250914886.html',
+    'https://inosmi.ru/20230504/ukraina-262710611.html',
+    'https://inosmi.ru/20230504/nato-262692864.html',
+    'https://inosmi.ru/20230505/konflikt-262704497.html',
+    'https://inosmi.ru/20230505/ukraina-262724526.html',
+]
 
 
 def read_charged_words():
@@ -29,17 +39,27 @@ async def fetch(session, url):
         return await response.text()
 
 
+async def process_article(session, morph, charged_words, url):
+    html = await fetch(session, url)
+    clean_text = adapters.SANITIZERS['inosmi_ru'](html, plaintext=True)
+    article_words = split_by_words(morph, clean_text)
+    score = calculate_jaundice_rate(article_words, charged_words)
+    words_count = len(article_words)
+
+    print()
+    print('URL:', url)
+    print('Рейтинг:', score)
+    print('Слов в статье:', words_count)
+
+
 async def main():
     morph = pymorphy2.MorphAnalyzer()
     charged_words = read_charged_words()
 
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'https://inosmi.ru/20211116/250914886.html')
-        clean_text = adapters.SANITIZERS['inosmi_ru'](html, plaintext=True)
-        article_words = split_by_words(morph, clean_text)
-        jaundice_rating = calculate_jaundice_rate(article_words, charged_words)
-        print(f'Рейтинг: {jaundice_rating}')
-        print(f'Слов в статье: {len(article_words)}')
+        async with create_task_group() as task_group:
+            for url in TEST_ARTICLES:
+                task_group.start_soon(process_article, session, morph, charged_words, url)
 
 
 if __name__ == '__main__':
