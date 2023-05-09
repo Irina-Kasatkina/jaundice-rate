a73e0ee44bc3da8b18a3bfac605b5d6b6d2d8719
Irina Kasatkina <kasatkina235@gmail.com>
2023-05-09 18:58:10 +0300

    Limit number of urls in request

--- server.py
+++ server.py
@@ -3,8 +3,15 @@ from aiohttp import web
 from articles_processor import process_articles
 
 
+MAX_COUNT_OF_URLS = 10
+
+
 async def handle(request):
     urls = request.query.get('urls', '').split(',')
+    if len(urls) > MAX_COUNT_OF_URLS:
+        error_message = f'too many urls in request, should be {MAX_COUNT_OF_URLS} or less'
+        return web.json_response({'error': error_message}, status=400)
+
     results = await process_articles(urls)
     return web.json_response(results)
 
